45fd9a0499903d00594df4f8660f67ccbbcb465d
askanade <kanadeashwini18@gmail.com>
2025-03-09 09:52:18 -0400

    Command Pattern and Plugins

---
+++ app/plugins/exit/__init__.py
@@ -0,0 +1,7 @@
+from app.commands import Command
+
+class ExitCommand(Command):
+    def execute(self):
+        print("Exiting...")
+        raise SystemExit
+    
--- main.py
+++ main.py
@@ -2,9 +2,9 @@
 Command-line interface for performing basic calculator operations.
 """
 
-import sys
 from calculator import Calculator
 from decimal import Decimal, InvalidOperation
+from app import App
 
 def calculate_and_print(operand1, operand2, operation_name):
     """
@@ -28,28 +28,30 @@ def calculate_and_print(operand1, operand2, operation_name):
 
         if operation_name in operation_mappings:
             result = operation_mappings[operation_name](operand1_decimal, operand2_decimal)
-            print(f"The result of {operand1} {operation_name} {operand2} is: {result}")
+            print(f"The result of {operand1} {operation_name} {operand2} is equal to {result}")
         else:
             print(f"Error: Unsupported operation '{operation_name}'. Available operations: add, subtract, multiply, divide.")
 
     except InvalidOperation:
-        print(f"Error: Invalid number input. '{operand1}' or '{operand2}' is not a valid number.")
+        print(f"Invalid number input: {operand1} or {operand2} is not a valid number.")
     except ZeroDivisionError:
         print("Error: Division by zero is not allowed.")
     except Exception as e:
         print(f"Unexpected error: {e}")
 
 def main():
-    """
-    Parses command-line arguments and executes the requested operation.
-    """
-    if len(sys.argv) != 4:
-        print(f"Usage: python {sys.argv[0]} <number1> <number2> <operation>")
-        print("Example: python main.py 10 5 add")
-        sys.exit(1)
+    # """
+    # Parses command-line arguments and executes the requested operation.
+    # """
+    # if len(sys.argv) != 4:
+    #     print(f"Usage: python {sys.argv[0]} <number1> <number2> <operation>")
+    #     print("Example: python main.py 10 5 add")
+    #     sys.exit(1)
 
-    _, operand1, operand2, operation = sys.argv
-    calculate_and_print(operand1, operand2, operation)
+    # _, operand1, operand2, operation = sys.argv
+    # calculate_and_print(operand1, operand2, operation)
+    app = App()  # Initialize the App class
+    app.start()
 
 if __name__ == '__main__':
     main()
